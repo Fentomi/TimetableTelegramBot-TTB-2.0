0729ff41b80dc6f3fc5666c4183989c61638c01d
Fentomi <105577819+Fentomi@users.noreply.github.com>
2022-09-19 11:19:13 +0300

    Add files via upload

--- parsers/yesterday_parser.py
+++ parsers/yesterday_parser.py
@@ -1,28 +1,17 @@
-import requests
+from parsers.func import html_get, repair_dayofweek, timetable_text_generator, save_file, dayweek_today
 from bs4 import BeautifulSoup
-import datetime
-import time
+import lxml
 
-url = 'https://ruz.narfu.ru/?timetable&group=16555'
-header = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.102 Safari/537.36 OPR/90.0.4480.100'}
-dayweek_today = time.strftime('%A', time.gmtime(time.time()))
-
-def parser_yesterday():
-    res = requests.get(url, headers=header)
-    res.encoding = 'utf-8'
-    if res.status_code == 200:
-        html = res.text
-    else:
-        with open(r'cache\timetable.html') as file:
-            html = file.read
+def parser_yesterday(url, header) -> str:
+    html = html_get(url, header)
     soup = BeautifulSoup(html, 'lxml')
-
     #если день недели суббота или воскресение
     if dayweek_today == 'Saturday': #если суббота, возвращает сообщение "на завтра расписания нет."
         return 'нет расписания на воскресение.'
     elif dayweek_today == 'Sunday': #если воскресение, то пытается взять следующую неделю и спарсить понедельник.
-        container = soup.find('div', class_='row tab-pane active').next_sibling
-        col = container.find('div', class_='list col-md-2')
+        container = soup.find('div', class_='row tab-pane active')
+        next_container = container.find_next_sibling('div', class_='row tab-pane')
+        col = next_container.find('div', class_='list col-md-2')
         paras = col.find_all('div', class_='timetable_sheet')
     elif dayweek_today == 'Friday': #если пятница, то пытается спарсить расписание на субботу
         container = soup.find('div', class_='row tab-pane active')
@@ -36,42 +25,15 @@ def parser_yesterday():
         paras = col.find_all('div', class_='timetable_sheet')
 
     dayofweek = col.find('div', class_='dayofweek').get_text()
-    dayofweek_split = dayofweek.split(',')
-    dayofweek_repair = ''
-    for i in dayofweek_split:
-        new = ''.join(i.split()) + ' '
-        dayofweek_repair += new
+    dayofweek_repair = repair_dayofweek(dayofweek)
     text = f'{dayofweek_repair}\n\n'
 
-    for para in paras:
-        num_para = para.find('span', class_='num_para').get_text()
-        time_para = 'пустая пара'
-        kindOfWork = ''
-        discipline = ''
-        auditorium = ''
-        group = ''
-        try:
-            time_para = para.find('span', class_='time_para').get_text()
-            kindOfWork = para.find('span', class_='kindOfWork').get_text()
-            discipline = para.find('span', class_='discipline').get_text()
-            auditorium_b = para.find('span', class_='auditorium').find('b').get_text()
-            auditorium_other = para.find('span', class_='auditorium').find('b').next_sibling
-            group = para.find('span', class_='group').get_text()
-            time_para = ''.join(time_para.split())
-            auditorium_other = ''.join(auditorium_other.split())
-            auditorium_other_split = auditorium_other.split(',')
-            auditorium = auditorium_b + ', ' + auditorium_other_split[-1]
-        except:
-            pass
-        if time_para != '' and discipline != '':
-            text += f'{num_para}) <u>[{time_para}]</u>\n{kindOfWork}\n<pre>{discipline}</pre>\n{auditorium}\n<i>{group}</i>\n\n'
-        else:
-            text += f'{num_para}) [{time_para}]\n\n'
+    paras = col.find_all('div', class_='timetable_sheet')
+    text = timetable_text_generator(paras=paras, text=text)
     
-    with open(r'cache\timetable.html', 'w', encoding='utf-8') as file:
-        file.write(html)
-    
+    save_file(html=html, text='timetable')
+
     return text
 
 if __name__ == '__main__':
-    print(parser_yesterday())
+    print(parser_yesterday('https://ruz.narfu.ru/?timetable&group=16555', {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.102 Safari/537.36 OPR/90.0.4480.100'}))
