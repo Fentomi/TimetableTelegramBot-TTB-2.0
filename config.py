97d7ae279f0836c8e6700c2aa53ff9cbc7c0c607
Nikita <105577819+Fentomi@users.noreply.github.com>
2023-08-28 12:45:42 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import time
 
-token = '5401569358:AAEyaEhSEubXcOkbtG0YrB6QbMi52O-u4jg'
-header = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.102 Safari/537.36 OPR/90.0.4480.100'}
+token = 'enter your token'
+header = {'User-Agent': 'enter your User-Agent'}
 url = r'https://ruz.narfu.ru/?timetable&group=16555'
 url_weather = r'https://www.gismeteo.ru/weather-severodvinsk-3914/'
 now_hours_time = int(time.strftime('%H', time.gmtime(time.time()))) + 3
